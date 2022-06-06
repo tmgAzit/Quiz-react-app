7a2f31e74aa312da03a99890263e9916d2b7ed28
tmgAzit <azit.ghising@gmail.com>
2022-06-06 13:35:35 +0930

    Handle change and handleSubmit is define.

--- src/context.js
+++ src/context.js
@@ -24,6 +24,12 @@ const AppProvider = ({ children }) => {
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
 
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  });
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const fetchQuestions = async (url) => {
@@ -55,12 +61,21 @@ const AppProvider = ({ children }) => {
     setIndex((oldIndex) => {
       const index = oldIndex + 1;
       if (index > questions.length - 1) {
-        // openModal
+        openModal();
         return 0;
       } else {
         return index;
       }
     });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setWaiting(true);
+    setIsModalOpen(false);
   };
 
   const checkAnswer = (value) => {
@@ -70,9 +85,23 @@ const AppProvider = ({ children }) => {
     nextQuestion();
   };
 
-  useEffect(() => {
-    fetchQuestions(tempURL);
-  }, []);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuiz({ ...quiz, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { amount, category, difficulty } = quiz;
+
+    // const tempURL =
+    //   'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
+
+    const url = `${API_ENDPOINT}amount=${amount}$category=${table[category]}&difficulty=${difficulty}&type=multiple`;
+
+    fetchQuestions(url);
+  };
 
   return (
     <AppContext.Provider
@@ -86,6 +115,10 @@ const AppProvider = ({ children }) => {
         isModalOpen,
         nextQuestion,
         checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
       }}
     >
       {children}
