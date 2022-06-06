6e332c6c16ff2a18db091a31935767856658fcfb
tmgAzit <azit.ghising@gmail.com>
2022-06-06 11:18:00 +0930

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
+function App() {
+  const { waiting, loading, questions, error } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm />;
+  }
+  if (loading) {
+    return <Loading />;
+  }
+  return <main>quiz app</main>;
+}
+
+export default App;
---
+++ src/context.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+};
+
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
+
+const url = '';
+
+const tempURL =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    try {
+      const response = await axios(url);
+      console.log(response);
+      if (response) {
+        const data = response.data.results;
+        if (data.length > 0) {
+          setQuestions(data);
+          setLoading(false);
+          setWaiting(false);
+          setError(false);
+        } else {
+          setLoading(false);
+          setWaiting(true);
+          setError(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempURL);
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
