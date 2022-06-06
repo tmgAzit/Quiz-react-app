8d2d69e397c6d188b5776c30215f6169f56b3e4c
tmgAzit <azit.ghising@gmail.com>
2022-06-06 14:07:56 +0930

    Random correct answer index is define.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     waiting,
     loading,
     questions,
-    error,
     correct,
     index,
     nextQuestion,
@@ -24,11 +23,19 @@ function App() {
   }
 
   const { question, correct_answer, incorrect_answers } = questions[index];
-  const answers = [...incorrect_answers, correct_answer];
+  // const answers = [...incorrect_answers, correct_answer];
+  const answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
 
   return (
     <main>
-      {/* <Modal /> */}
+      <Modal />
       <section className='quiz'>
         <p className='correct-answers'>
           correct answers : {correct}/{index}
